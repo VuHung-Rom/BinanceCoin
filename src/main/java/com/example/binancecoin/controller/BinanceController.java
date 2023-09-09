695bbd9c96d5b5d91176e520fddd65e1189ced33
hungvv <hungvv@biplus.com.vn>
2023-09-10 00:41:19 +0700

    reformat code

--- src/main/java/com/example/binancecoin/controller/BinanceController.java
+++ src/main/java/com/example/binancecoin/controller/BinanceController.java
@@ -29,7 +29,7 @@ import java.util.*;
 @RestController
 public class BinanceController {
 
-    private static  Map<String, List<Cachedata>> MAP_CACHE_VOL_DATA  = null;
+    private static Map<String, List<Cachedata>> MAP_CACHE_VOL_DATA = null;
     @Autowired
     private CacheDataRepository cacheDataRepository;
 
@@ -69,28 +69,28 @@ public class BinanceController {
     }
 
     @GetMapping("/api/v3")
-    public ResponseEntity<List<Cachedata>> getHistory( @RequestParam String coin, String dataRange) {
-         int totalRow=2;
-        if(MAP_CACHE_VOL_DATA == null){
+    public ResponseEntity<List<Cachedata>> getHistory(@RequestParam String coin, String dataRange) {
+        int totalRow = 2;
+        if (MAP_CACHE_VOL_DATA == null) {
             MAP_CACHE_VOL_DATA = new HashMap<>();
             // TODO: lần đầu tiên thì load du lieu tu DB len
 
             //Lay danh sach ca ban ghi trong db ra
-            List<CacheDataEntity > lstData =
+            List<CacheDataEntity> lstData =
                     cacheDataRepository.findAll(Sort.by(Sort.Direction.ASC, "token")
                             .and(Sort.by(Sort.Direction.ASC, "openTime")));
 
             //Voi tung ban ghi tu db
-            for (CacheDataEntity cacheDataEntity:lstData ) {
-                List<Cachedata>  lst = MAP_CACHE_VOL_DATA.get(cacheDataEntity.getToken());
+            for (CacheDataEntity cacheDataEntity : lstData) {
+                List<Cachedata> lst = MAP_CACHE_VOL_DATA.get(cacheDataEntity.getToken());
                 //Neu ma coin chua co list trong MAP_CACHE_VOL_DATA thi can khoi tao list moi
-                if(lst == null){
+                if (lst == null) {
                     lst = new ArrayList<>();
-                    MAP_CACHE_VOL_DATA.put(cacheDataEntity.getToken(),lst);
+                    MAP_CACHE_VOL_DATA.put(cacheDataEntity.getToken(), lst);
                 }
                 //Neu ma coin da co list trong MAP_CACHE_VOL_DATA thi lay list ra va them vao
                 Cachedata cachedata = new Cachedata();
-                BeanUtils.copyProperties(cacheDataEntity,cachedata);
+                BeanUtils.copyProperties(cacheDataEntity, cachedata);
                 lst.add(cachedata);
 
             }
@@ -99,13 +99,13 @@ public class BinanceController {
 
         List<Cachedata> listData = MAP_CACHE_VOL_DATA.get(coin);
 
-        if(listData !=null && new Date().getTime() - listData.get(listData.size()-1).getCloseTime().getTime() < 300000)
-            return  ResponseEntity.status(HttpStatus.OK).body(listData);
+        if (listData != null && new Date().getTime() - listData.get(listData.size() - 1).getCloseTime().getTime() < 300000)
+            return ResponseEntity.status(HttpStatus.OK).body(listData);
 
-        if( listData == null){
+        if (listData == null) {
             listData = new ArrayList<>();
-            MAP_CACHE_VOL_DATA.put(coin,listData);
-            totalRow= 1000;
+            MAP_CACHE_VOL_DATA.put(coin, listData);
+            totalRow = 1000;
         }
 
         BinanceClient binanceClient = Feign.builder()
@@ -118,13 +118,13 @@ public class BinanceController {
 
         List<List<String>> binanceDataChart = binanceClient.getHistory(totalRow, coin, "5m");
 
-        for (int i=0; i< binanceDataChart.size()-1; i++ ) {
-            List<String> dataPoint =binanceDataChart.get(i);
+        for (int i = 0; i < binanceDataChart.size() - 1; i++) {
+            List<String> dataPoint = binanceDataChart.get(i);
             Cachedata cahedata = new Cachedata();
             cahedata.setVolume(Double.valueOf(dataPoint.get(5)));
             cahedata.setOpenTime(new Date(Long.valueOf(dataPoint.get(0))));
             cahedata.setCloseTime(new Date(Long.valueOf(dataPoint.get(6))));
-            if(listData.size()== 0 || cahedata.getOpenTime().after(listData.get(listData.size()-1).getOpenTime())){
+            if (listData.size() == 0 || cahedata.getOpenTime().after(listData.get(listData.size() - 1).getOpenTime())) {
                 listData.add(cahedata);
                 //TODO: save du lieu vao db
                 CacheDataEntity cacheDataEntity = new CacheDataEntity();
@@ -138,13 +138,14 @@ public class BinanceController {
         return ResponseEntity.status(HttpStatus.OK).body(listData);
 
     }
-    @Scheduled(fixedRate = 300000)
+
+
     @GetMapping("/api/v3/deth")
     public ResponseEntity<?> getDeth(@RequestParam String coin) {
-
-        GetDepthRespose cachedeth ;
         DepthVol depthVol = new DepthVol();
-        int totalRow = 2000;
+        int totalRow = 5000;
+        // TODO: cache lại dữ liệu
+
         BinanceClient binanceClient = Feign.builder()
                 .client(new OkHttpClient())
                 .encoder(new GsonEncoder())
@@ -153,64 +154,61 @@ public class BinanceController {
                 .logLevel(Logger.Level.FULL)
                 .target(BinanceClient.class, "https://www.binance.com");
         GetDepthRespose binanceClientDepth = binanceClient.getDepth(coin, totalRow);
-        cachedeth = binanceClientDepth;
 
-        if (cachedeth != null) {
+        if (binanceClientDepth != null) {
             //TODO:
             // Tính tổng vol +5%, -5%
 
             Double max = null;
             Double min = null;
-            for (Double price : binanceClientDepth.getAsks().keySet())
-            {
-                if(min==null){
-                    min =price;
-                }else {
-                    if(min>price){
-                        min =price;
+            for (Double price : binanceClientDepth.getAsks().keySet()) {
+                if (min == null) {
+                    min = price;
+                } else {
+                    if (min > price) {
+                        min = price;
                     }
                 }
 
             }
-            for (Double price : binanceClientDepth.getBids().keySet()){
-                if(max==null){
-                    max =  price;
-                }else {
-                    if(max< price){
+            for (Double price : binanceClientDepth.getBids().keySet()) {
+                if (max == null) {
+                    max = price;
+                } else {
+                    if (max < price) {
                         max = price;
 
                     }
                 }
             }
-            Double matchPrice = (max +min)/2;
+            Double matchPrice = (max + min) / 2;
 
-            Double fivebuy= 0D;
-            for (Double price : binanceClientDepth.getAsks().keySet()){
+            Double fivebuy = 0D;
+            for (Double price : binanceClientDepth.getAsks().keySet()) {
                 Double vol = binanceClientDepth.getAsks().get(price);
-                if(price<(1.05*matchPrice)){
-                fivebuy += vol;
+                if (price < (1.05 * matchPrice)) {
+                    fivebuy += vol;
                 }
             }
-            Double haibuy= 0D;
-            for (Double price : binanceClientDepth.getAsks().keySet()){
+            Double haibuy = 0D;
+            for (Double price : binanceClientDepth.getAsks().keySet()) {
                 Double vol = binanceClientDepth.getAsks().get(price);
-                if(price<(1.02*matchPrice)){
+                if (price < (1.02 * matchPrice)) {
                     haibuy += vol;
                 }
             }
 
-            Double fiveSell= 0D;
-            for (Double price : binanceClientDepth.getBids().keySet()){
+            Double fiveSell = 0D;
+            for (Double price : binanceClientDepth.getBids().keySet()) {
                 Double vol = binanceClientDepth.getBids().get(price);
-                if(price>(0.95*matchPrice)){
+                if (price > (0.95 * matchPrice)) {
                     fiveSell += vol;
                 }
             }
-
-            Double haiSell= 0D;
-            for (Double price : binanceClientDepth.getBids().keySet()){
+            Double haiSell = 0D;
+            for (Double price : binanceClientDepth.getBids().keySet()) {
                 Double vol = binanceClientDepth.getBids().get(price);
-                if(price>(0.98*matchPrice)){
+                if (price > (0.98 * matchPrice)) {
                     haiSell += vol;
                 }
             }
